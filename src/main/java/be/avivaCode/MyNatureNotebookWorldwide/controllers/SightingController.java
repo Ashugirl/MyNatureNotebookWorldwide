e523d55f8eefbefd5a8f9d64721c31b55f1b3988
Ashugirl <aviva.shuman@gmail.com>
2022-11-18 19:41:24 +0100

    cleaned up unused code

--- src/main/java/be/avivaCode/MyNatureNotebookWorldwide/controllers/SightingController.java
+++ src/main/java/be/avivaCode/MyNatureNotebookWorldwide/controllers/SightingController.java
@@ -73,75 +73,9 @@ public class SightingController {
     public String home(Model model){
         List<Sighting> sightings = sightingService.getAllSightings();
         model.addAttribute("sightings", sightings);
-        System.out.println("hi. Do you see the sightings?");
         return "index";
     }
-//    @GetMapping("/index/showAllSightings")
-//    public String showAllSightings(Model model){
-//        List<Sighting> sightings = sightingService.getAllSightings();
-//        model.addAttribute("sightings", sightings);
-//        System.out.println("hi. Do you see the sightings?");
-//        return "showAllSightings";
-//    }
-//
-//    @GetMapping("/addSighting")
-//    public String showAddSightingForm(Model model){
-//        System.out.println("Hello? This is the sighting Controller. Show my form!");
-//        Sighting sighting = new Sighting();
-//        Map<String, String> speciesAutofill = new HashMap<>();
-//        Map<String, String> speciesNames = sightingService.getSearchedCommonNames("");
-//        for(Map.Entry<String, String> entry : speciesNames.entrySet()){
-//            String scientificName = entry.getKey();
-//            String commonNames = entry.getValue();
-//            speciesAutofill.put(scientificName, commonNames);
-//                                }
-//       // User user = userServiceImpl.getCurrentUser(userId);
-//        model.addAttribute("sighting", sighting);
-//        model.addAttribute("speciesAutofill", speciesAutofill);
-////        model.addAttribute("countryList", sightingService.getCountryList());
-////        model.addAttribute("deceased", sighting.isDeceased());
-////        model.addAttribute("lifer", sighting.getLifer());
-////        model.addAttribute("locationHidden", new Sighting().isLocationHidden());
-////        model.addAttribute("keepPrivate", new Sighting().isKeepPrivate());
-//        return "addSighting";
-//    }
-//
-//
-//
-//   @PostMapping("/addSighting")
-//    public String submitSighting(@ModelAttribute("sighting") Sighting sighting, BindingResult bindingResult, Long id, String speciesCommonName) {
-//       System.out.println("Hello from submit Sighting in Sighting controller");
-//        Optional<User> user = userServiceImpl.getUserById(id);
-//        sighting = new Sighting();
-//        sightingService.createSighting(sighting);
-////        sighting = new Sighting(user, speciesCommonName);
-////       // sighting = new Sighting(sighting.getSightingId(), user, sighting.getContinent(), sighting.getCountry(), "" );
-////        sightingService.createSighting(sighting);
-//////        model.addAttribute("user", userServiceImpl.getCurrentUser(user.getUserId()));
-//////        model.addAttribute("sighting", sightingService.createSighting(sighting));
-////
-//       return "redirect:/sighting";
-////
-//    }
-//
-////    @RequestMapping("/addSighting")
-////    public String addSighting(Model model, Sighting sighting, BindingResult bindingResult){
-////        if(bindingResult.hasErrors())
-////            return "addSighting";
-////        model.addAttribute("sighting", sighting);
-////        return "addSighting";
-////    }
-////
-////    @GetMapping("/commonNameAutocomplete")
-////    @ResponseBody
-////    public List<String> commonNameAutocomplete(@RequestParam(value = "term", required = false, defaultValue = "")String term){
-////        List<String> suggestions = new ArrayList<>();
-////        suggestions.add("Red Maple");
-////        suggestions.add("Red Squirrel");
-////        suggestions.add("Red Fox");
-////        suggestions.add("Red Snapper");
-////        return suggestions;
-////    }
+
 }
 
 
